45188c9aab7e36acb033ab4c9cdfa5ed0d0afe52
Vedrana Tozija <vedrana.tozija@gmail.com>
2013-05-17 12:14:00 +0200

    Vedrana update1
    Slikite rabotat

---
+++ Memory/Memory/Square.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.ComponentModel;
+using System.Data;
+using System.Windows.Forms;
+
+
+namespace Memory
+{
+    class Square 
+    {
+        public bool isSelected { get; set; }
+        public Square pair { get; set; }
+        public Button button;
+        public List<Square> vedrana;
+
+        public Square(Square p, Button b)
+        {
+            isSelected = false;
+            pair = p;
+            button = b;
+            vedrana.Add(p);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
